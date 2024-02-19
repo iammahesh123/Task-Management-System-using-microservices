658c6cb508e45188c150cd935666e4ee7356bef0
KADAMBALA MAHESH <maheshkadambala18@gmail.com>
2024-02-19 19:35:28 +0530

    Delete ZipkinProject directory

--- ZipkinProject/src/main/java/com/example/zipkinproject/UserController.java
+++
@@ -1,8 +0,0 @@
-package com.example.zipkinproject;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class UserController {
-
-}
--- ZipkinProject/src/main/java/com/example/zipkinproject/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.zipkinproject;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends MongoRepository<User, String> {
-}
